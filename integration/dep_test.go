484d00cb2679e61abc02578bacf97d52e70f0b88
Forest Eckhardt <feckhardt@pivotal.io>
2020-05-29 11:29:48 -0400

    Adds integration testing

---
+++ integration/dep_test.go
@@ -0,0 +1,80 @@
+package integration_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+	. "github.com/paketo-buildpacks/occam/matchers"
+)
+
+func testDep(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
+
+		pack   occam.Pack
+		docker occam.Docker
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack()
+		docker = occam.NewDocker()
+	})
+
+	context("when building a go app using dep", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+
+			name string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+		})
+
+		it("creates a working OCI image", func() {
+			var err error
+			var logs fmt.Stringer
+			image, logs, err = pack.WithNoColor().Build.
+				WithBuildpacks(goBuildpack).
+				WithNoPull().
+				Execute(name, filepath.Join("testdata", "dep"))
+			Expect(err).NotTo(HaveOccurred(), logs.String())
+
+			container, err = docker.Container.Run.Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(BeAvailable(), ContainerLogs(container.ID))
+
+			response, err := http.Get(fmt.Sprintf("http://localhost:%s", container.HostPort()))
+			Expect(err).NotTo(HaveOccurred())
+			defer response.Body.Close()
+
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+
+			content, err := ioutil.ReadAll(response.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(content)).To(ContainSubstring("Hello, World!"))
+			Expect(string(content)).To(MatchRegexp("PATH=.*/layers/paketo-buildpacks_dep/app-binary/bin:"))
+
+			Expect(logs).To(ContainLines(ContainSubstring("Go Compiler Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Dep Buildpack")))
+		})
+	})
+}
---
+++ scripts/integration.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+set -eu
+set -o pipefail
+
+readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly BUILDPACKDIR="$(cd "${PROGDIR}/.." && pwd)"
+
+# shellcheck source=.util/tools.sh
+source "${PROGDIR}/.util/tools.sh"
+
+# shellcheck source=.util/print.sh
+source "${PROGDIR}/.util/print.sh"
+
+function main() {
+  if [[ ! -d "${BUILDPACKDIR}/integration" ]]; then
+      util::print::warn "** WARNING  No Integration tests **"
+  fi
+
+  tools::install
+  images::pull
+  tests::run
+}
+
+function tools::install() {
+  util::tools::pack::install \
+      --directory "${BUILDPACKDIR}/.bin"
+
+  util::tools::jam::install \
+      --directory "${BUILDPACKDIR}/.bin"
+}
+
+function images::pull() {
+  util::print::title "Pulling build image..."
+  docker pull "${CNB_BUILD_IMAGE:=gcr.io/paketo-buildpacks/build:full-cnb-cf}"
+
+  util::print::title "Pulling run image..."
+  docker pull "${CNB_RUN_IMAGE:=gcr.io/paketo-buildpacks/run:full-cnb-cf}"
+
+  util::print::title "Pulling cflinuxfs3 builder image..."
+  docker pull "${CNB_BUILDER_IMAGE:=gcr.io/paketo-buildpacks/builder:cflinuxfs3}"
+
+  export CNB_BUILD_IMAGE
+  export CNB_RUN_IMAGE
+  export CNB_BUILDER_IMAGE
+
+  util::print::title "Setting default pack builder image..."
+  pack set-default-builder "${CNB_BUILDER_IMAGE}"
+}
+
+function tests::run() {
+  util::print::title "Run Buildpack Runtime Integration Tests"
+  pushd "${BUILDPACKDIR}" > /dev/null
+      if GOMAXPROCS=4 go test -timeout 0 ./integration/... -v -run Integration; then
+          util::print::success "** GO Test Succeeded **"
+      else
+          util::print::error "** GO Test Failed **"
+      fi
+  popd > /dev/null
+}
+
+main "${@:-}"

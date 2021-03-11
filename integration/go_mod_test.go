2868552d3f9bff149ca558716117922d6f823f01
Ryan Moran <rmoran@vmware.com>
2021-03-10 17:01:07 -0800

    Fixes Go Mod Vendor tests of vendored app

--- integration/go_mod_test.go
+++ integration/go_mod_test.go
@@ -122,4 +122,58 @@ func testGoMod(t *testing.T, context spec.G, it spec.S) {
 			})
 		})
 	})
+
+	context("when building a go mod app that is vendored", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+
+			name   string
+			source string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+
+			source, err = occam.Source(filepath.Join("testdata", "go_mod_vendored"))
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
+		})
+
+		it("creates a working OCI image", func() {
+			var err error
+			var logs fmt.Stringer
+			image, logs, err = pack.WithNoColor().Build.
+				WithBuildpacks(goBuildpack).
+				WithPullPolicy("never").
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred(), logs.String())
+
+			container, err = docker.Container.Run.
+				WithEnv(map[string]string{"PORT": "8080"}).
+				WithPublish("8080").
+				WithPublishAll().
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(container).Should(Serve(ContainSubstring("Hello, World!")).OnPort(8080))
+
+			Expect(logs).To(ContainLines(ContainSubstring("Go Distribution Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Go Mod Vendor Buildpack")))
+			Expect(logs).To(ContainLines(ContainSubstring("Go Build Buildpack")))
+
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Dep Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Procfile Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Environment Variables Buildpack")))
+			Expect(logs).NotTo(ContainLines(ContainSubstring("Image Labels Buildpack")))
+		})
+	})
 }

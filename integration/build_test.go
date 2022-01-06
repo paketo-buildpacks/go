dc7538d212ec5ef324b7c34653ee2a30480e3cf3
Frankie Gallina-Jones <frankieg@vmware.com>
2022-01-06 10:15:43 -0500

    update deprecated function calls

--- integration/build_test.go
+++ integration/build_test.go
@@ -208,7 +208,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 						"PORT":                 "8080",
 						"SERVICE_BINDING_ROOT": "/bindings",
 					}).
-					WithVolume(fmt.Sprintf("%s:/bindings/ca-certificates", filepath.Join(source, "binding"))).
+					WithVolumes(fmt.Sprintf("%s:/bindings/ca-certificates", filepath.Join(source, "binding"))).
 					Execute(image.ID)
 				Expect(err).NotTo(HaveOccurred())
 

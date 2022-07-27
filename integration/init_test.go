7152c1111129d9233408f09dd81c3f8354bae23f
Frankie Gallina-Jones <frankieg@vmware.com>
2022-07-27 10:04:45 -0400

    add bionic tiny builder to test set
    don't interate over builders inside test suite

--- integration/init_test.go
+++ integration/init_test.go
@@ -1,8 +1,6 @@
 package integration_test
 
 import (
-	"encoding/json"
-	"os"
 	"os/exec"
 	"path/filepath"
 	"testing"
@@ -16,18 +14,8 @@ import (
 
 var goBuildpack string
 
-var config struct {
-	Builders []string `json:"builders"`
-}
-
 func TestIntegration(t *testing.T) {
 	Expect := NewWithT(t).Expect
-
-	file, err := os.Open("../integration.json")
-	Expect(err).NotTo(HaveOccurred())
-
-	Expect(json.NewDecoder(file).Decode(&config)).To(Succeed())
-	Expect(file.Close()).To(Succeed())
 
 	output, err := exec.Command("bash", "-c", "../scripts/package.sh --version 1.2.3").CombinedOutput()
 	Expect(err).NotTo(HaveOccurred(), string(output))

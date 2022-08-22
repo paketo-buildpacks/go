798171c74bc24a58d02252f5ef0a5e65fc160896
Forest Eckhardt <forest.eckhardt@gmail.com>
2022-08-22 15:14:16 +0000

    Removes dep and dep-ensure

--- integration/reproducible_builds_test.go
+++ integration/reproducible_builds_test.go
@@ -74,55 +74,6 @@ func testReproducibleBuilds(t *testing.T, context spec.G, it spec.S) {
 		})
 	})
 
-	context("when building a go app using dep", func() {
-		var (
-			image occam.Image
-
-			name   string
-			source string
-		)
-
-		it.Before(func() {
-			var err error
-			name, err = occam.RandomName()
-			Expect(err).NotTo(HaveOccurred())
-
-			source, err = occam.Source(filepath.Join("testdata", "dep"))
-			Expect(err).NotTo(HaveOccurred())
-		})
-
-		it.After(func() {
-			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
-			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
-			Expect(os.RemoveAll(source)).To(Succeed())
-		})
-
-		it("creates a two identical images from the same input", func() {
-			var err error
-			var logs fmt.Stringer
-			image, logs, err = pack.WithNoColor().Build.
-				WithBuildpacks(goBuildpack).
-				WithPullPolicy("never").
-				Execute(name, source)
-			Expect(err).NotTo(HaveOccurred(), logs.String())
-
-			firstID := image.ID
-
-			// Delete the first image
-			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
-			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
-
-			image, logs, err = pack.WithNoColor().Build.
-				WithBuildpacks(goBuildpack).
-				WithPullPolicy("never").
-				WithClearCache().
-				Execute(name, source)
-			Expect(err).NotTo(HaveOccurred(), logs.String())
-
-			Expect(firstID).To(Equal(image.ID))
-		})
-	})
-
 	context("when building a go app using go mod", func() {
 		var (
 			image occam.Image

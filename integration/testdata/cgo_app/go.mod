8a0a2cde9bfff307817dd4a848877bf555394d82
Sophie Wigmore <swigmore@vmware.com>
2022-09-27 16:29:13 -0400

    Use mattn/go-sqlite3 module in app to test CGO

---
+++ integration/testdata/cgo_app/go.mod
@@ -0,0 +1,14 @@
+module go-online
+
+go 1.18
+
+require (
+	github.com/BurntSushi/toml v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.15
+	github.com/satori/go.uuid v1.2.0
+)
+
+require (
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+)
---
+++ integration/testdata/cgo_app/site.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	_ "embed"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	uuid "github.com/satori/go.uuid"
+
+	"github.com/BurntSushi/toml"
+)
+
+//go:embed .occam-key
+var s string
+
+type Config struct {
+	Age int
+}
+
+func main() {
+	moonPtr := flag.Bool("moon", false, "say Hello, Moon!")
+	flag.Parse()
+
+	var conf Config
+	if _, err := toml.Decode("whatever", &conf); err != nil {
+		fmt.Println("toml library installed")
+	}
+
+	u2 := uuid.NewV4()
+	fmt.Printf("UUIDv4: %s\n", u2)
+
+	http.HandleFunc("/", hello(*moonPtr))
+	fmt.Println("listening...")
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	fmt.Println("setting up db")
+	if err != nil {
+		panic(fmt.Errorf("sql err: %v", err))
+	}
+	defer db.Close()
+	fmt.Println("db closed")
+
+	port := "8080"
+	if systemPort := os.Getenv("PORT"); systemPort != "" {
+		port = systemPort
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		panic(err)
+	}
+}
+
+func hello(moon bool) func(res http.ResponseWriter, req *http.Request) {
+	if moon {
+		return func(res http.ResponseWriter, req *http.Request) {
+			fmt.Fprint(res, "Hello, Moon!")
+		}
+	}
+	return func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(res, "Hello, World!")
+	}
+}
